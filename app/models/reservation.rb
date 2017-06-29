ce63741acbaab066912cb6a6e9f80289112b4366
Quinton Boltin <quinton@valleyexpetec.com>
2017-06-29 22:40:42 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ActiveRecord::Base
   belongs_to :guest, class_name: "User"
-  belongs_to :trip, class_name: "Reservation"
   belongs_to :listing
-  has_one :review, foreign_key: "reservation_id"
+  has_one :review
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ActiveRecord::Base
-  belongs_to :listing
   belongs_to :guest, class_name: "User"
   belongs_to :reservation
 end
