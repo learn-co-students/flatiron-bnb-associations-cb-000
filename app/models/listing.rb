9139c0b8957767be2682ac1ae360d4e5b4e3243f
sjmoody <steven@beachheadmarketing.com>
2017-11-13 17:14:24 +0100

    passing 5 of 29.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ActiveRecord::Base
+  belongs_to :neighborhood
+
+  belongs_to :host, :class_name => "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :listings, :foreign_key => 'host_id'
+  has_many :reservations, :foreign_key => 'guest_id'
+  has_many :reviews, through: :reservations, :foreign_key => 'guest_id'
 end
